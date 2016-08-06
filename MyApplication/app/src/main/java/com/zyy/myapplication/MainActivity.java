67f1874c0750850dce1f750965e15602439c1768
e <767030703@qq.com>
2016-08-06 16:57:34 +0800

    delete zhengyanyan

--- MyApplication/app/src/main/java/com/zyy/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/zyy/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //fsfjlsjflsjfsj
-    //zhengyanyan
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
